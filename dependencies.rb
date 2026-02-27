01273bbb7beb2d7ad89d505a023660029ea3f592
Jean-Philippe <jpduchesne89@gmail.com>
2026-02-27 17:04:56 -0500

    Add ruby field to dev.yml
    This generates the shadowenv for said ruby

--- dependencies.rb
+++ dependencies.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 # Prerequisites when running tests for managed repos (Ruby ecosystem).
-# Used by bin/test.rb and dev up. Shadowenv uses this to generate .shadowenv.d/510_ruby.lisp.
+# Used by bin/test.rb and dev up.
 
 BUNDLER_VERSION = "~> 2.1"
-
-# Ruby version for this project (used by shadowenv generator and .ruby-version).
-RUBY_VERSION_REQUESTED = "4.0.1"
