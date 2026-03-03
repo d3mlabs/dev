4bf3bff5d4109ea8c26ec370a7f7eb435ac89c72
Jean-Philippe <jpduchesne89@gmail.com>
2026-03-03 08:55:13 -0500

    Remove dead code: Deps::VERSION

--- lib/dev/deps.rb
+++ lib/dev/deps.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require_relative "deps/version"
 require_relative "deps/config"
 require_relative "deps/brew"
 require_relative "deps/taps"
--- lib/dev/deps/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Dev
-  module Deps
-    VERSION = File.read(File.expand_path("../../../VERSION", __dir__)).strip
-  end
-end
